95a40267feff19c3250cca84d1059466e4d5a73f
pavel <nesoglasnyj.pavel@gmail.com>
2022-11-17 17:11:25 +0300

    знакомство и инициализация локального стэйта

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import './App.scss';
 import Todolist from './Todolist';
 
+
+
+
+//  function Counter(){
+//     let arr = useState(5)
+//     let data = arr[0];
+//     let setData = arr[1]
+   
+//     return <div onClick={() => {setData(data + 1)}}>{data}</div>
+// }
+
+
 function App() {
-    let tasks1 = [
+    let initTasks = [
         { id: 1, title: "HTML&CSS", isDone: true },
         { id: 2, title: "JS", isDone: true },
         { id: 3, title: "ReactJS", isDone: false },
@@ -15,15 +27,24 @@ function App() {
         { id: 2, title: "because", isDone: true },
         { id: 3, title: "I dont have good salary", isDone: false },
     ]
+
+        let arr = useState(initTasks)
+        let tasks = arr[0];
+        let setTasks = arr[1];
+        console.log(tasks)
+
     function removeTask(id: number) {
-        tasks1 = tasks1.filter(t => t.id !== id) 
-        console.log(tasks1);
+        let filteredTask = tasks.filter(t => t.id !== id)
+        setTasks(filteredTask)
+        console.log(filteredTask);
     }
+    
+
     return (
         <div className='App'>
             <Todolist 
             title="What to learn" 
-            tasks={tasks1} 
+            tasks={tasks} 
             removeTask={removeTask}/>
             <Todolist 
             title="How do you feel ?" 
