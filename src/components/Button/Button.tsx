024a8f22e94e8c040194dddc8e491c4546fe6c23
pavel <nesoglasnyj.pavel@gmail.com>
2023-03-14 15:09:48 +0300

    рефакторинг

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,13 +4,14 @@ import s from "./Button.module.scss";
 type propsButtonType = {
 	name: string
 	callBack: ()=> void
+	className: string
 }
 
 export const Button = (props: propsButtonType) => {
 
 	return (
 		<div>
-			<button className={s.btn} onClick={props.callBack}> {props.name} </button>
+			<button className={s.btn} onClick={props.callBack}> {props.name}</button>
 		</div>
 	)
 }
