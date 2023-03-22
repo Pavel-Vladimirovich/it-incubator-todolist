705693e0f3c83348c23a865272b31073b780f7a6
pavel <nesoglasnyj.pavel@gmail.com>
2023-03-22 17:11:09 +0300

    16 - React кабзда как подробно: useReducer

--- src/components/AddItemForm/AddItemForm.tsx
+++ src/components/AddItemForm/AddItemForm.tsx
@@ -1,56 +1,100 @@
-import React, {ChangeEvent, KeyboardEvent, useState} from "react";
+import React, { ChangeEvent, KeyboardEvent, useState, useReducer } from "react";
 import style from "./AddItemForm.module.scss";
 import { v1 } from "uuid";
+import { error } from "console";
 
 type AddItemFormType = {
-    addItem: (title: string) => void
-}
+  addItem: (title: string) => void;
+};
 
-export const AddItemForm = ({addItem}: AddItemFormType) => {
-    const [title, setTitle] = useState("");
+type ActionType = {
+  type: string;
+};
 
-    const [error, setError] = useState<string | null>(null);
+type StateType = {
+  error: string;
+};
 
-    const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
-        setTitle(event.currentTarget.value);
-    };
-    const onKeyPressHandler = (event: KeyboardEvent<HTMLInputElement>) => {
-        setError(null);
-        if (event.key === "Enter") {
-            addTasksHandler();
-            setTitle("");
-        }
-    };
+const TOGGLE_ERROR = "TOGGLE-ERROR";
+const ERROR_FIELD_IS_EMPTY = "ERROR-FIELD-IS-EMPTY";
+const ERROR_LETTERS_MORE_THAN = "ERROR-LETTERS-MORE-THAN";
 
-    const addTasksHandler = () => {
-        if (title.trim() === "") {
-            setError("Field can't be empty");
-            return;
-        }
-        if (title.length > 15) {
-            setError("No more than 10 letters");
-            return;
-        }
-        addItem(title.trim());
-        setTitle("");
-    };
-    const htmlForm = v1();
-    return (
-        <div className={`${style.text_field} ${style.text_field_floating_2}`}>
-            {/*{error ? (<span className={style.error_message}>{error}</span>) : ''}*/}
-            <input
-                id={htmlForm}
-                placeholder='Enter text'
-                value={title}
-                className={`${style.text_field__input} ${error ? style.error : ""}`}
-                onChange={onChangeHandler}
-                onKeyDown={onKeyPressHandler}
-            />
-            <label htmlFor={htmlForm} className={`${style.text_field__label} ${error ? style.error : ""}`}>{error ? `${error}` : 'Enter text'}</label>
-            <button
-                className={`${style.btn} ${style.btn_input}`}
-                onClick={addTasksHandler}>add
-            </button>
-        </div>
-    )
-}
+const reducer = (state: StateType, action: ActionType): StateType => {
+
+  switch (action.type) {
+    case TOGGLE_ERROR:
+     return {
+        ...state,
+        error: state.error = ''
+     };
+    case ERROR_FIELD_IS_EMPTY:
+      return {
+        ...state,
+        error: state.error = "Field can't be empty"
+      };
+    case ERROR_LETTERS_MORE_THAN:
+      return{
+        ...state,
+        error: state.error = "No more than 15 letters"
+      }
+    default:
+      throw new Error("Bad action type");
+  }
+};
+
+export const AddItemForm = ({ addItem }: AddItemFormType) => {
+  const [state, dispatch] = useReducer(reducer, { error: ""});
+
+  const [title, setTitle] = useState("");
+
+  // const [error, setError] = useState<string | null>(null);
+
+  const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.currentTarget.value);
+  };
+  const onKeyPressHandler = (event: KeyboardEvent<HTMLInputElement>) => {
+    dispatch({ type: TOGGLE_ERROR });
+    if (event.key === "Enter") {
+      addTasksHandler();
+      setTitle("");
+    }
+  };
+
+  const addTasksHandler = () => {
+    if (title.trim() === "") {
+      dispatch({ type: ERROR_FIELD_IS_EMPTY });
+      return;
+    }
+    if (title.length > 15) {
+      dispatch({ type: ERROR_LETTERS_MORE_THAN });
+      return;
+    }
+    addItem(title.trim());
+    setTitle("");
+  };
+  const htmlForm = v1();
+  return (
+    <div className={`${style.text_field} ${style.text_field_floating_2}`}>
+      <input
+        id={htmlForm}
+        placeholder="Enter text"
+        value={title}
+        className={`${style.text_field__input} ${state.error ? style.error : ""}`}
+        onChange={onChangeHandler}
+        onKeyDown={onKeyPressHandler}
+      />
+      <label
+        htmlFor={htmlForm}
+        className={`${style.text_field__label} ${state.error ? style.error : ""}`}
+      >
+        {state.error ? `${state.error}` : "Enter text"}
+      </label>
+      <button
+        className={`${style.btn} ${style.btn_input}`}
+        onClick={addTasksHandler}
+      >
+        add
+      </button>
+    </div>
+  );
+};
