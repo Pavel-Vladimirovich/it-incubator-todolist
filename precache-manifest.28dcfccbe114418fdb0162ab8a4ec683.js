a052217f85a46d0427e14457f31de208f74990a5
pavel <nesoglasnyj.pavel@gmail.com>
2023-06-14 16:37:51 +0300

    Updates

---
+++ precache-manifest.28dcfccbe114418fdb0162ab8a4ec683.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e9f1cb1ddcaec6e0b8758ba0ab7a00a",
+    "url": "/it-incubator-todolist/index.html"
+  },
+  {
+    "revision": "c66dc7efc2cedf4fd5b5",
+    "url": "/it-incubator-todolist/static/css/main.d1b67611.chunk.css"
+  },
+  {
+    "revision": "5f40f303813cc8bc34c0",
+    "url": "/it-incubator-todolist/static/js/2.fd3983ac.chunk.js"
+  },
+  {
+    "revision": "58427e4729239bfadd784204e9ef1054",
+    "url": "/it-incubator-todolist/static/js/2.fd3983ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c66dc7efc2cedf4fd5b5",
+    "url": "/it-incubator-todolist/static/js/main.97611051.chunk.js"
+  },
+  {
+    "revision": "db2169c340d8e0ccd066",
+    "url": "/it-incubator-todolist/static/js/runtime-main.e679c243.js"
+  }
+]);
