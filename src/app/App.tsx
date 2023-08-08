19ba3d9fd240440d0b87e27e9f666bafc62c45e1
Pavel-Vladimirovich <nesoglasnyj.pavel@gmail.com>
2023-08-08 22:51:22 +0300

    refactoring login form

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,7 +8,7 @@ import {Outlet} from "react-router-dom";
 
 
 function App() {
-    console.log("render app")
+    // console.log("render app")
     return (
         <>
             <ThemeProvider theme={theme}>
--- src/features/Login/Login.tsx
+++ src/features/Login/Login.tsx
@@ -1,78 +1,52 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import {
   Button,
   Card,
   CardContent,
   Checkbox,
+  FormControl,
   FormControlLabel,
+  FormGroup, FormHelperText,
+  FormLabel,
   Grid,
   TextField,
-  Typography,
+  Typography
 } from "@material-ui/core";
-import AccountCircle from "@material-ui/icons/AccountCircle";
-import { makeStyles } from "@material-ui/core/styles";
+import {makeStyles} from "@material-ui/core/styles";
 
 const useStyles = makeStyles({
-  root: {
-    minWidth: 375,
-  },
-  container: {
-    // border: '1px solid grey',
-    marginTop: " 50px",
-  },
+    container: {
+        marginTop: " 90px",
+    },
 });
 
 export const Login = () => {
-  const classes = useStyles();
-  const [values, setValues] = useState<any>({
-    amount: "",
-    password: "",
-    weight: "",
-    weightRange: "",
-    showPassword: false,
-  });
+    const classes = useStyles();
+    const [values, setValues] = useState<any>({
+        password: "",
+    });
 
-  const handleChange =
-    (prop: any) => (event: React.ChangeEvent<HTMLInputElement>) => {
-      setValues({ ...values, [prop]: event.target.value });
-    };
-
-  const handleClickShowPassword = () => {
-    setValues({ ...values, showPassword: !values.showPassword });
-  };
-
-  const handleMouseDownPassword = (
-    event: React.MouseEvent<HTMLButtonElement>
-  ) => {
-    event.preventDefault();
-  };
-  return (
-    <>
-      <form noValidate autoComplete="off">
-        <Grid container justifyContent="center" className={classes.container}>
-          <Card variant="outlined" className={classes.root}>
-            <CardContent>
-              <Grid container direction="column" spacing={2}>
-                <Grid item>
-                  <TextField label="Enter your email" fullWidth required />
-                </Grid>
-                <Grid item>
-                  <TextField type="password" label="Enter your pass" fullWidth required
-                  />
-                </Grid>
-                <Grid item>
-                  <FormControlLabel control={<Checkbox color="primary"/> } label={<Typography color="textSecondary">Remember me</Typography>}/>
-                </Grid>
-                <Grid item>
-                  <Button color="primary" variant="outlined" fullWidth>
-                    Enter
-                  </Button>
-                </Grid>
-                </Grid>
-            </CardContent>
-          </Card>
-        </Grid>
-      </form>
-    </>
-  );
+    return (
+        <>
+          <Grid container justifyContent="center" className={classes.container}>
+            <Grid item lg={4} sm={6} xs={12}>
+              <form noValidate autoComplete="off" onSubmit={(e)=>{e.preventDefault()}}>
+                <FormControl fullWidth>
+                  <FormLabel>Sign in:</FormLabel>
+                  <FormGroup>
+                    <TextField label="Enter your email"/>
+                    <FormHelperText>email</FormHelperText>
+                    <TextField label="Enter your pass"/>
+                    <FormHelperText>password</FormHelperText>
+                    <FormControlLabel control={<Checkbox color="primary"/>}
+                                      label={<Typography color="textSecondary">Remember
+                                        me</Typography>}/>
+                    <Button type={"submit"} color="primary" variant="outlined" fullWidth>sign in</Button>
+                  </FormGroup>
+                </FormControl>
+              </form>
+            </Grid>
+          </Grid>
+        </>
+    );
 };
