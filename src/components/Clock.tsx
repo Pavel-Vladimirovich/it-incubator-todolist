739630662f7f426c765bbc0bdb356b17e229e104
Pavel-Vladimirovich <nesoglasnyj.pavel@gmail.com>
2023-08-15 23:05:10 +0300

    refactoring. practice

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -1,24 +1,37 @@
 import React, { useEffect, useState } from "react";
+import {Grid, Typography} from "@material-ui/core";
 
 
 export const Clock = () => {
 
+	const [time, setTime] = useState(new Date())
 
-	const [time, setTime] = useState(1)
+	const options: any = {
+		era: 'long',
+		year: 'numeric',
+		month: 'long',
+		day: 'numeric',
+		weekday: 'long',
+		timezone: 'UTC',
+		hour: 'numeric',
+		minute: 'numeric',
+		second: 'numeric'
+	};
+
 
 
 	useEffect(()=>{
 		setInterval(()=>{
-			setTime(state => state + 1)
-		}, 1000)	
+			setTime(new Date())
+		}, 1000)
 	}, [])
 	
 
 
 	return(
-		<>
-			{time}
-		</>
+		<Grid container justifyContent="center">
+			<Grid item><Typography variant="h4" color="textSecondary">{time.toLocaleString("ru", options)}</Typography></Grid>
+		</Grid>
 	)
 }
 
